022951f3243d73e46e95289471496dea00585eda
Baskar <baskar.official@outlook.in>
2023-05-31 00:34:46 +0530

    get started form UI done

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const router = require("./routes");
+app.use("/", router);
+app.use("*", router);
+
 app.listen(3000, () => {
   console.log("server is running");
 });

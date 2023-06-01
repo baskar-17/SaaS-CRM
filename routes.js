fcd374059cbc9dfbc6f4246dc10d14ff45440d70
Baskar <baskar.official@outlook.in>
2023-06-02 00:16:26 +0530

    created UI components

--- routes.js
+++ routes.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const ObjectId = require("mongoose").ObjectId;
+const Callbacks = require("./model/Callbacks");
 const router = express.Router();
 
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+//Users
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.post("/requestcallback", (req, res) => {
+  const { name, email, phone } = req.body;
+  const callback = new Callbacks({
+    name: name,
+    email: email,
+    phone: phone,
+    date: new Date(),
+  });
+  callback.save();
+  res.redirect("/");
+});
+
+router.get("/admincallbacks", async (req, res) => {
+  const admincallbacks = await Callbacks.find().sort({ date: "desc" });
+  res.render("./admin/admincallbacks", { admincallbacks: admincallbacks });
+});
+
+router.post("/updatecallbacks", (req, res) => {
+  let admincallbacks = Callbacks.findOne({ _id: ObjectId(req.body.id) });
+  console.log(admincallbacks);
+  // res.redirect("/updatecallbacks");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 router.get("*", (req, res) => {
   res.render("notFound");
 });
 
+//Admin
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const ejs = require("ejs");
-
+const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 const router = require("./routes");
+const Users = require("./model/Users");
+const Callback = require("./model/Callbacks");
+
+mongoose
+  .connect(
+    "mongodb+srv://baskars:Baskar1997@cluster0.okkvo6e.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("mongoDB connected");
+  });
+
 app.use("/", router);
+app.use("/requestcallback", router);
+app.use("/admincallbacks", router);
+app.use("/updatecallbacks", router);
+app.use("/login", router);
 app.use("*", router);
 
 app.listen(3000, () => {
